ab4311a4ee355a63871a7c93bd5b7882d258b14e
mgradec1 <mgradec1@uwyo.edu>
2017-02-08 02:38:39 -0700

    changed tracking to a more advanced module
    do not attempt to build this with the default binaries, this requires custom binaries to be installed

--- VisionMouse/mouse.cpp
+++ VisionMouse/mouse.cpp
@@ -1,105 +1,75 @@
-#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/utility.hpp>
+#include <opencv2\tracking.hpp>
 #include <Windows.h>
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/objdetect.hpp"
-
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <stdio.h>
 using namespace cv;
 using namespace std;
 
-CascadeClassifier face_cascade;
-int iLastX = -1;
-int iLastY = -1;
-int totalY = GetSystemMetrics(SM_CXSCREEN); //get total size in pixels of screen's height
-int totalX = GetSystemMetrics(SM_CYSCREEN); //get total size in pixels of screen's width
-
-void detectAndDisplay(Mat frame)
+int main(int argc, char *argv[])
 {
-	std::vector<Rect> faces;
-	Mat frame_gray;
-	cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
-	equalizeHist(frame_gray, frame_gray);
-	//-- Detect faces
-	face_cascade.detectMultiScale(frame_gray, faces, 2, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
 	
-	for (size_t i = 0; i < faces.size(); i++) {
-		Point center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2);
-		ellipse(frame, center, Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
-		Mat faceROI = frame_gray(faces[i]);
+	Mat frame;
+	int key = 0;
+	/* initialize camera */
+	VideoCapture cap;
+	cap.open(0);
 
-		if (center.x > iLastX + 7) {
-		SetCursorPos(center.x + (totalX/4), center.y);
-		}
-		if (center.x < iLastX - 7) {
-			SetCursorPos(center.x - (totalX / 4), center.y);
-		}
-		if (center.y > iLastY + 7) {
-			SetCursorPos(center.x, center.y + (totalY / 4));
-		}
-		if (center.y > iLastX - 7) {
-			SetCursorPos(center.x, center.y - (totalY / 4));
-		}
-
-
-		iLastX = center.x;
-		iLastY = center.y;
-	}
-}
-
-
-int main(int argc, char** argv){
-	VideoCapture cap(0); //capture the video from webcam
-	String face_cascadeName = "C:\\OpenCV-3.1.0\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_alt.xml";
-	
-	bool start = true; //flag for seting the cursor to middle of screen on start
-
-	if (!cap.isOpened())  // if not success, exit program
-	{
-		cout << "Cannot open the web cam" << endl;
-		return -1;
+	/* always check */
+	if (!cap.isOpened()) {
+		printf("Cannot open initialize webcam!\n");
+		exit(0);
 	}
 
-	if (!face_cascade.load(face_cascadeName)) {
-		printf("--(!)Error loading face cascade\n"); 
-		return -1;
-	}
-
+	/* create a window for the video */
+	cvNamedWindow("result", CV_WINDOW_AUTOSIZE);
 	
+		/* get a frame */
+		cap.read(frame);
+		/* always check */
+		if (frame.empty()) {
+			cerr << "error blank frame";
+			return 0;
+		} 
 
-	//Capture a temporary image from the camera
-	Mat imgTmp;
-	cap.read(imgTmp);
+		/* sets the Region of Interest*/
+		Rect2d roi;
 
-	
-	while (true)
-	{
-		Mat imgOriginal;
+		//create tracker
+		Ptr<Tracker> tracker = Tracker::create("KCF");
 
-		bool bSuccess = cap.read(imgOriginal); // read a new frame from video
-
-		if (!bSuccess) //if not success, break loop
-		{
-			cout << "Cannot read a frame from video stream" << endl;
-			break;
-		}
+		cap >> frame;
+		roi = selectROI("tracker", frame);
 		
-		if (start == true) {
-			SetCursorPos(totalX, totalY);
-			start = false;
+		tracker->init(frame, roi);
+
+		while (true) {
+			//get frame from the camera
+			cap >> frame;
+
+			//if the frame is empty quit
+			if (frame.empty()) {
+				break;
+			}
+			//update the tracker
+			tracker->update(frame, roi);
+			//note left and right are flipped
+			SetCursorPos(roi.x * 3, roi.y *3);
+			
+			//draw the tracking square
+			rectangle(frame, roi, Scalar(255, 0, 0), 2, 1);
+
+			//show the frame
+			imshow("Live", frame);
+
+			if (waitKey(1) == 27) {
+				break;
+			}
 		}
-
-		detectAndDisplay(imgOriginal);
-
-		imshow("Original", imgOriginal); //show the original image
-
-		if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
-		{
-			cout << "esc key is pressed by user" << endl;
-			break;
-		}
-	}
-
+		/* display current frame */
 	return 0;
 }
-
 
