de8e1607ceaafa3aa5c7c57a774aaca85825ccdc
mgradec1 <mgradec1@uwyo.edu>
2017-05-05 08:18:16 -0600

    Create mouseDriver.cc

---
+++ FinalVersion/Voice/mouseDriver.cc
@@ -0,0 +1,21 @@
+//  mouseDriver.cc
+//  Authored by Mark A. Gradecki & Cody Fagley
+//  February 13, 2017
+//  Senior Design: MouseVision
+
+//  Contains a test driver for the MouseFunct class
+
+#include <iostream>
+
+#include "mouseFunct.h"
+
+//  main returns 0 if successful
+int main ()
+{
+    //  Tracking Variables
+    int rtn;
+
+    mouseFunct();
+
+    return 0;
+}
